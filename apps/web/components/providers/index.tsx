d5bd7ffcb06b0f22638b7a32f091fc34aa857c39
irere123 <irere2020@outlook.com>
2026-02-12 21:19:57 +0200

    fix: build time env vars

--- apps/web/components/providers/index.tsx
+++ apps/web/components/providers/index.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-import { PrivyProvider } from "@privy-io/react-auth";
-
 export function Providers({ children }: { children: React.ReactNode }) {
-	const privyAppId = process.env.NEXT_PUBLIC_PRIVY_APP_ID ?? "";
-
-	return <PrivyProvider appId={privyAppId}>{children}</PrivyProvider>;
+	return <>{children}</>;
 }
